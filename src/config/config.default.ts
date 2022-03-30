58e671415bcd28fa2ca3e8622c08568e2012e95e
wei <guangchaowei66@163.com>
2022-03-30 15:06:10 +0800

    modify environment profile

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -19,25 +19,22 @@ export default (appInfo: EggAppInfo) => {
     replaceEggLogger: true,
   };
 
-  // config.security = {
-  //   csrf: false,
-  // };
+  // cors
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
+  };
+
+  // global error handler
+  config.onerror = {
+    all(err, ctx) {
+      // 在此处定义针对所有响应类型的错误处理方法
+      // 注意，定义了 config.all 之后，其他错误处理方法不会再生效
+      loggers.getLogger('logger').warn('url: %s, error is: %s', ctx.originalUrl, err)
+      ctx.body = JSON.stringify(ResultVO.error('The server is busy'));
+      ctx.status = 500;
+    },
+  };
 
   return config;
-};
-
-// global error handler
-export const onerror = {
-  all(err, ctx) {
-    // 在此处定义针对所有响应类型的错误处理方法
-    // 注意，定义了 config.all 之后，其他错误处理方法不会再生效
-    loggers.getLogger('logger').warn('url: %s, error is: %s', ctx.originalUrl, err)
-    ctx.body = JSON.stringify(ResultVO.error('The server is busy'));
-    ctx.status = 500;
-  },
-};
-
-export const cors = {
-  origin: '*',
-  allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
 };
