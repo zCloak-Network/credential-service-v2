c37c748b55b9c1b5256f21426bc5d1d83fc872aa
whgreate <624906158@qq.com>
2022-06-20 15:45:07 +0800

    remove useless code

--- src/configuration.ts
+++ src/configuration.ts
@@ -6,15 +6,12 @@ import * as typegoose from '@midwayjs/typegoose';
 import * as swagger from '@midwayjs/swagger';
 import Web3 from 'web3';
 import * as orm from '@midwayjs/orm';
-import { AppInitializerHelper } from './framework/AppInitializerHelper';
+// import { AppInitializerHelper } from './framework/AppInitializerHelper';
 import { AdminAttesterService } from './service/AdminAttesterService';
+import { SubmitAttestationTaskInitializer } from './framework/impl/SubmitAttestationTaskInitializer';
 
 @Configuration({
-  imports: [
-    typegoose,
-    swagger,
-    orm,
-  ],
+  imports: [typegoose, swagger, orm],
   importConfigs: [join(__dirname, './config')],
   conflictCheck: true,
 })
@@ -29,12 +26,15 @@ export class ContainerLifeCycle implements ILifeCycle {
   adminAttesterService: AdminAttesterService;
 
   async onReady(container: IMidwayContainer) {
+    console.log(`Current ENVIRONMENT: ${this.app.getEnv()}`);
+
     // inject web3
     const web3 = new Web3(this.chainUrl);
     container.registerObject('web3', web3);
 
-    await AppInitializerHelper.init(this.app);
+    // await AppInitializerHelper.init(this.app);
 
-    console.log(`Current ENVIRONMENT: ${this.app.getEnv()}`);
+    // 不要await
+    new SubmitAttestationTaskInitializer().doInit(this.app);
   }
 }
